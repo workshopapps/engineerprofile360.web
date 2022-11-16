c91ed02c64ad839654b3dd5f946f352201d1adf1
boluajileye <boluwatifeajileye@gmail.com>
2022-11-16 23:50:47 +0100

    Feature Login, Logout, Refresh implemented.

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+        $this->middleware('auth:api', ['except' => ['login']]);
     }
 
     public function login(Request $request)
