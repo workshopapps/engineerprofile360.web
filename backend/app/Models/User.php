7f7cd8585f31f06d3fa229ec826c7b88a79c8316
boluajileye <boluwatifeajileye@gmail.com>
2022-11-17 00:15:09 +0100

    Feature Login, Logout, Refresh implemented....

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
