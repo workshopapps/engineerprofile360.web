e94cdc221a0fdbafa3a384594dedec07ffb7a387
dreywandowski <aduramimo@gmail.com>
2022-11-17 18:13:19 +0100

    error and success response

--- backend/app/Http/Controllers/AssessmentController.php
+++ backend/app/Http/Controllers/AssessmentController.php
@@ -4,21 +4,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Assessment;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class AssessmentController extends Controller
 {
     
 
     // @dreywandowski ---- delete an assessment
-    public function deleteAss($ass_id){
+    public function deleteAss($ass_id): JsonResponse{
         try{
         $response = Assessment::findorFail($ass_id); 
         }
         catch(ModelNotFoundException $e){
-            return sendResponse(true, 404, "no records found for id ".$ass_id,[] );
+            return $this->errorResponse(
+                'no records found for id '.$ass_id,
+                'Record not found',
+                Response::HTTP_NOT_FOUND
+            );
         }
         $response->delete();   
-        return sendResponse(false, 200, "deleted successfully ", []);
+        return $this->successResponse(
+            true,
+            'deleted successfully',
+            'this has been deleted',
+            Response::HTTP_OK
+        );
     }
     /////////////////////////////////////////////////////
 }
