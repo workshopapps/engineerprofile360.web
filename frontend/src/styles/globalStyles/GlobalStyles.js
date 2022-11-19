5f9330a5d8e190617d1409fe139ec439670aca9b
ricqCodes <princenwakanma1996@gmail.com>
2022-11-20 03:42:22 +0800

    Added fontfamily to global styles

--- frontend/src/styles/globalStyles/GlobalStyles.js
+++ frontend/src/styles/globalStyles/GlobalStyles.js
@@ -8,6 +8,7 @@ const GlobalStyles = createGlobalStyle`
     * {
         padding: 0;
         margin: 0;
+        font-family: 'Inter', sans-serif;
         box-sizing: border-box;
         position: relative;
     }
