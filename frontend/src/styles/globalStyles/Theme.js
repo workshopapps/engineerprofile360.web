ea22f6c97ab3779cc177239ddd42a131d0a46af9
ricqCodes <princenwakanma1996@gmail.com>
2022-11-20 03:50:39 +0800

    Added colors to theme

--- frontend/src/styles/globalStyles/Theme.js
+++ frontend/src/styles/globalStyles/Theme.js
@@ -3,57 +3,57 @@ const theme = {
     divider: "#EDEBE9",
     border: {
       default: "#8A8886",
-      hover: "#323130"
+      hover: "#323130",
     },
     main: {
       primary: {
-        primary: "",
-        lighterAlt: "",
-        lighter: "",
-        light: ""
+        primary: "#141AE9",
+        lighterAlt: "#EFF6FC",
+        lighter: "#DEECF9",
+        light: "#C7E0F4",
       },
       tertiary: {
-        tertiary: "",
-        darkAlt: "",
-        darker: "",
-        light: ""
-      }
+        tertiary: "#3D94DF",
+        darkAlt: "#1E78C6",
+        dark: "#1166A6",
+        darker: "#0D4E7E",
+      },
     },
     status: {
       error: {
-        background: "",
-        color: ""
+        background: "#FDE7E9",
+        color: "#A80000",
       },
       success: {
-        background: "",
-        color: ""
+        background: "#DFF6DD",
+        color: "#107C10",
       },
       warning: {
-        background: "",
-        color: ""
+        background: "#FFF4CE",
+        color: "#797775",
       },
       severeWorking: {
-        background: "",
-        color: ""
-      }
+        background: "#FED9CC",
+        color: "#D83B01",
+      },
     },
     overlay: {
       light: "rgba(255, 255, 255, 0.4)",
-      dark: "rgba(0, 0, 0, 0.4)"
+      dark: "rgba(0, 0, 0, 0.4)",
     },
     grey: {
       white: "#FFFFFF",
       shades: {
-        10: "",
-        20: "",
-        30: "",
-        60: "",
-        90: "",
-        130: "",
-        160: "",
-        190: ""
-      }
-    }
+        10: "#FAF9F8",
+        20: "#F3F2F1",
+        30: "#EDEBE9",
+        60: "#C8C6C4",
+        90: "#A19F9D",
+        130: "#605E5C",
+        160: "#323130",
+        190: "#201F1E",
+      },
+    },
   },
 
   spacing: (num) => {
