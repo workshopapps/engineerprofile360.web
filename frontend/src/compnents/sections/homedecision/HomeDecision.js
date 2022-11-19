3418833e1294f8444254fa76459fcd6f0918f0f4
OsujiPius <piusosuji9@gmail.com>
2022-11-19 13:45:05 +0100

    feat: landing page

---
+++ frontend/src/compnents/sections/homedecision/HomeDecision.js
@@ -0,0 +1,24 @@
+import React from "react"
+import styles from "../homedecision/HomeDecision.module.css"
+import Preview from "../../../assets/images/preview.svg"
+
+export default function Homedecision() {
+  return (
+    <div className={styles.decision_container}>
+      <img src={Preview} alt="" />
+      <h1>Make decisions in a flash</h1>
+      <p>
+        Our assessment tests give engineers and their managers a clear glimpse
+        of where they are and what they need to move to the next level of
+        growth. No <br /> longer do managers need to excessively make use of
+        limited time to make important decisions and with a clear overview of
+        the team’s abilities, <br /> engineering management decisions can be
+        made faster.
+      </p>
+      <div className={styles.cta_container}>
+        <button className={styles.button}>Request a demo</button>
+        <a href="#/">Let's chat?</a>
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
+import Homedecision from "../compnents/sections/homedecision/HomeDecision"
+import HomeHero from "../compnents/sections/homeHero/HomeHero"
+import HomeStats from "../compnents/sections/homeStats/HomeStats"
+import styles from "../globalStyles/Global.module.css"
 
 export default function Home() {
   return (
     <div>
-      <h1>Axle</h1>
+      <HomeHero />
+      <HomeStats />
+      <Homedecision />
     </div>
   )
 }
