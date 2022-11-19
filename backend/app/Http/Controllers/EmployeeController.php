3ea5e4e6dff05bf124c22623595376c24bf1e160
chisainty <johnchinedudivine@gmail.com>
2022-11-19 16:51:51 +0100

    fix: updated user scores controller methods

--- backend/app/Http/Controllers/EmployeeController.php
+++ backend/app/Http/Controllers/EmployeeController.php
@@ -14,7 +14,7 @@ class EmployeeController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
     
     public function addEmpCSV(Request $request)
