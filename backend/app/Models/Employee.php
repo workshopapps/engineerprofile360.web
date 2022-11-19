939fd3de13da40e651d686bda0eb07e485fa323f
dasaint001 <elijahbamidele48@yahoo.com>
2022-11-19 13:09:45 +0100

    fix: added routes to the swagger documentation and fix models fillable fields

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -11,7 +11,12 @@ class Employee extends Model
     use HasFactory, Uuids;
 
     //role-incase 
+
+    /**
+     * @var $fillable
+    */
     protected $fillable = [
-        'email', 'fullname', 'username' 
+        'email', 'fullname', 'username', 'hash', 'image',
+        'occupation', 'reftoken', 'role', 'org_id'
     ];
 }
