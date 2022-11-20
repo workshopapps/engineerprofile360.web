0b5ddafb822d0af8f9fe474bb63263f60978441c
oaikhienancornelius97 <corneliusedos@gmail.com>
2022-11-20 07:11:57 -0800

    Feat: Terms and service page without header and footer component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "../src/styles/Global.module.css"
+import './index.css'
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
