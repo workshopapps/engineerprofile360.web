c3eee33854ec323c361b00ed80ce5bcb5873c1f7
OsujiPius <piusosuji9@gmail.com>
2022-11-20 20:27:16 +0100

    feat: support page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,11 @@ import React from "react"
 import { ThemeProvider } from "styled-components"
 import { Route, Routes } from "react-router-dom"
 import Home from "../src/main/pages"
-import UserSupport from "./ui/pages/UserSupport"
 import Confirmed from "./main/compnents/demo-pages-components/components/Confirmed"
 import ScheduleDemo from "./main/compnents/demo-pages-components/components/ScheduleDemo"
-import Confirmed from "./main/compnents/demo-pages-components/components/ScheduleDemo"
 import { GlobalStyles, theme } from "./styles/globalStyles"
 import Contact from "../src/main/pages/Contact"
+import Support from "../src/ui/pages/UserSupport"
 import UserProfile from "./ui/pages/user-profile/UserProfile"
 
 const App = () => {
@@ -19,6 +18,7 @@ const App = () => {
         <Route path="/demo" element={<ScheduleDemo />} />
         <Route path="/confirm-demo" element={<Confirmed />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
       {/* <UserProfile /> */}
     </ThemeProvider>
