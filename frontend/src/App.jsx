707717178c48a9325a1ae5076df4c99cc2162506
ricqCodes <princenwakanma1996@gmail.com>
2022-11-21 04:36:15 +0800

    Added Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import { Route, Routes } from "react-router-dom"
-import Home from "../src/main/pages"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./main/pages"
+import Confirmed from "./main/compnents/demo-pages-components/components/Confirmed"
 import ScheduleDemo from "./main/compnents/demo-pages-components/components/ScheduleDemo"
-import Confirmed from "./main/compnents/demo-pages-components/components/ScheduleDemo"
 import { GlobalStyles, theme } from "./styles/globalStyles"
-import Contact from "../src/main/pages/Contact"
+import Contact from "./main/pages/Contact"
 // import UserProfile from "./ui/pages/user-profile/UserProfile"
+import UiLayout from "./ui/components/UiLayout"
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/demo" element={<ScheduleDemo />} />
-        <Route path="/confirm-demo" element={<Confirmed />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<ScheduleDemo />} />
+          <Route path="/confirm-demo" element={<Confirmed />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <UiLayout>INSERT PAGES FOR APP </UiLayout> */}
+        </Routes>
+      </BrowserRouter>
       {/* <UserProfile /> */}
     </ThemeProvider>
   )
