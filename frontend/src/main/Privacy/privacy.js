39d5444ea5f9c5404994000fb999248ea2a3b170
EzehVictoria <ezehvictoria86@gmail.com>
2022-11-20 13:42:27 +0100

    feet:Privacy policy - #31

---
+++ frontend/src/main/Privacy/privacy.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import MainContent from "./components/MainContent";
+import Table from "./components/Table";
+
+
+const Privacy = () => {
+    return ( 
+        <Fragment>
+            <Table />
+            <MainContent /> 
+        </Fragment>
+
+     );
+}
+ 
+export default Privacy;
