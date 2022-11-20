b77955d700a257ec105d7068561713fa5f5a2e0f
Kojo Baffoe <kojobaffoe935@gmail.com>
2022-11-20 11:11:06 -0500

    feat: create assessment manually page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import Home from "../src/main/pages"
 import CreateAssessmentManually from "./ui/pages/CreateAssessmentManually"
+import "../src/styles/Global.module.css"
 import UserSupport from "./ui/pages/UserSupport"
 
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
-      <UserSupport />
-      {/* <CreateAssessmentManually /> */}
+      {/* <UserSupport /> */}
+      <CreateAssessmentManually />
     </div>
   )
 }
