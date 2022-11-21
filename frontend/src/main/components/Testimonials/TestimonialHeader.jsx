fb9e5aee8429976fd608dcc02643c81ec42783f4
unknown <wyze97@gmail.com>
2022-11-21 15:29:07 +0100

    testimonial header picture added

---
+++ frontend/src/main/components/Testimonials/TestimonialHeader.jsx
@@ -0,0 +1,7 @@
+import tesimonialpicture from "./assets/images/testimonialHead.png"
+
+export default function TestimonialHeader(){
+    <div>
+        <img src={tesimonialpicture} alt="picture" />
+    </div>
+}
