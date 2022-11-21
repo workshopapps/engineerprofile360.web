b8f412550f5fee5c6772aeebb81309e3611b0da2
unknown <wyze97@gmail.com>
2022-11-21 15:38:51 +0100

    Added the ovierview component

---
+++ frontend/src/main/components/Testimonials/TestimonialOverview.jsx
@@ -0,0 +1,9 @@
+export default function TestimonialOverview(){
+    return(
+        <div>
+            <h1>Testimonials</h1>
+            <h1>Our customers love what we do</h1>
+            <p>Over 500 companies and individuals use Skript to improve team work and get more opportunities. We make sure our customers always come first and the last thank you comes from us.</p>
+        </div>
+    )
+}
