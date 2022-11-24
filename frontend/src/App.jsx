af28dec6cb3f95773a4ef17bca372f0c8e1af533
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-11-25 00:19:15 +0100

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,22 @@ import MainLayout from "./main/Layouts/Main/MainLayout";
 
 import Contact from "./main/pages/Contact";
 import { AdminAuthLayout } from "./main/components";
-import { Home, About, AdminSignup, AdminLogin, ResetPassword, ResetPasswordSuccess, VerifyEmail } from "./main/pages";
+import {
+  Home,
+  About,
+  AdminSignup,
+  AdminLogin,
+  ResetPassword,
+  ResetPasswordSuccess,
+  VerifyEmail,
+} from "./main/pages";
 import Confirmed from "./main/components/demo-pages-components/components/Confirmed";
 import ScheduleDemo from "./main/components/demo-pages-components/components/ScheduleDemo";
 
 import Support from "../src/ui/pages/UserSupport";
-import Terms from "../src/ui/pages/TermsAndService/TermsAndService";
+import Terms from "./ui/pages/termsAndService/TermsAndService.jsx";
 
-import UserProfile from "./ui/pages/user-profile/UserProfile";
+// import UserProfile from "./ui/pages/user-profile/UserProfile";
 
 import Blog from "../src/main/pages/Blog";
 import AssessmentList from "./main/components/sections/userAssessmentList/AssessmentList";
@@ -22,8 +30,9 @@ import UserTakeAssessment from "./main/components/sections/userTakeAssessment/Us
 import Privacy from "./main/pages/Privacy/privacy";
 import EmployeeProfile from "./ui/pages/EmployeeProfile/EmployeeProfile";
 import AdminSetting from "./ui/pages/AdminSetting/AdminSetting";
-import Testimonial from "./main/components/Testimonials/Testimonial";
+// import Testimonial from "./main/components/Testimonials/Testimonial";
 import HelpCenter from "../src/main/pages/HelpCenter";
+import Market from "./main/components/Market-comp-page/Market";
 
 const App = () => {
   return (
@@ -36,7 +45,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/demo" element={<ScheduleDemo />} />
           <Route path="/confirm-demo" element={<Confirmed />} />
- 
+
           <Route path="/support" element={<Support />} />
           <Route path="/termsAndService" element={<Terms />} />
           <Route path="/blog" element={<Blog />} />
@@ -53,7 +62,8 @@ const App = () => {
           />
           <Route path="/setting" element={<AdminSetting />} />
           <Route path="/privacy-policy" element={<Privacy />} />
-          <Route path="/employee-profile" element={<EmployeeProfile />} /> 
+          <Route path="/employee-profile" element={<EmployeeProfile />} />
+          <Route path="/Marketing-Comparison" element={<Market />} />
         </Route>
 
         {/* Auth routes */}
@@ -61,10 +71,12 @@ const App = () => {
           <Route path="/register" element={<AdminSignup />} />
           <Route path="/login" element={<AdminLogin />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/reset-password-success" element={<ResetPasswordSuccess />} />
+          <Route
+            path="/reset-password-success"
+            element={<ResetPasswordSuccess />}
+          />
           <Route path="/verify-email" element={<VerifyEmail />} />
         </Route>
-
       </Routes>
 
       {/* <UserProfile /> */}
---
+++ frontend/src/main/components/Market-comp-page/Market.styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 65%;
+  margin: 0 auto;
+  padding: 2rem 0;
+`;
+
+export const FirstPage = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+export const Left = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 25px;
+  margin-top: 20rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+export const Text1 = styled.h1`
+  width: 100%;
+  height: 90px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 70px;
+  line-height: 90px;
+  letter-spacing: 0.01em;
+  color: #323130;
+`;
+export const Textvs = styled.h2`
+  width: 82px;
+  height: 90px;
+  font-family: "Farro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 72px;
+  line-height: 90px;
+  letter-spacing: 0.01em;
+  color: #323130;
+  margin-left: 17rem;
+`;
+export const Right = styled.div`
+  width: 40%;
+  height: 100%;
+  padding: 25px;
+  margin-top: 20rem;
+`;
+export const SideImg = styled.img`
+  width: 200rem;
+`;
