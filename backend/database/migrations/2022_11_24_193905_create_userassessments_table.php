cee0b7d3700b3e51cb0f8081e10323eaaacdfa1a
princessvincent <priscavincent2018@gmail.com>
2022-11-24 12:09:18 -0800

    created controller and migration

---
+++ backend/database/migrations/2022_11_24_193905_create_userassessments_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userassessments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer("completed")->length(1)->default(0);
+            $table->integer("total_questions")->nullable();
+            $table->integer("correct_questions")->nullable();
+            $table->integer("result")->nullable();
+            $table->foreignUuid('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->onDelete('cascade');
+            $table->foreignUuid('assessment_id')
+                ->references('id')
+                ->on('assessments')
+                ->onDelete('cascade');
+            $table->foreignUuid('org_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('cascade');
+            $table->foreignUuid('userscore_id')
+                ->references('id')
+                ->on('user_scores')
+                ->onDelete('cascade');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userassessments');
+    }
+};
