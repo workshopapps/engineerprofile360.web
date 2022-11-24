72e2f74129c3e483db0fbf9fc21b8df3374b94e6
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-11-24 15:47:03 -0800

    fix: added middleware to add router

--- backend/database/migrations/2022_11_24_164952_create_stacks_table.php
+++ backend/database/migrations/2022_11_24_164952_create_stacks_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('stacks', function (Blueprint $table) {
             $table->uuid('id');
             $table->string('name');
-
             $table->timestamps();
         });
     }
