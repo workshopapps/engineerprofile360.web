9c0f101edec70a1896ab8824f6746758b2cbf9f6
benrobo <alumonabenaiah71@gmail.com>
2022-11-24 13:24:07 -0800

    fix(BE): fixed cors issues

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', "http://skript.hng.tech", "https://skript.hng.tech", "http://localhost:3000"],
 
     'allowed_origins_patterns' => [],
 
