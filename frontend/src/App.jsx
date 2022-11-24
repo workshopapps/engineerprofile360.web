3f3a1c633fd252baee48c90ca836680c4d7bb684
Oaikhienan Cornelius <37479800+oaikhienancornelius97@users.noreply.github.com>
2022-11-24 13:10:11 +0100

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,57 +2,74 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, theme } from "./styles/globalStyles";
-import MainLayout from "./main/Layouts/MainLayout";
+import MainLayout from "./main/Layouts/Main/MainLayout";
 
 import Contact from "./main/pages/Contact";
-import { About } from "./main/pages";
-import Home from "../src/main/pages/Home";
+import { AdminAuthLayout } from "./main/components";
+import { Home, About, AdminSignup, AdminLogin, ResetPassword, ResetPasswordSuccess, VerifyEmail } from "./main/pages";
 import Confirmed from "./main/components/demo-pages-components/components/Confirmed";
 import ScheduleDemo from "./main/components/demo-pages-components/components/ScheduleDemo";
+
 import Support from "../src/ui/pages/UserSupport";
-import Terms from "../src/ui/pages/termsAndService/TermsAndService";
+import Terms from "../src/ui/pages/TermsAndService/TermsAndService";
 
 import UserProfile from "./ui/pages/user-profile/UserProfile";
-import Register from "../src/main/components/sections/adminSignup/AdminSignup";
-import Login from "../src/main/components/sections/adminLogin/AdminLogin";
-import Blog from "../src/main/pages/Blog";
-import BlogStory from "../src/main/components/blog/Blogstory";
-import AssessmentList from "./main/components/sections/userAssessmentList/AssessmentList";
-import UserTakeAssessment from "./main/components/sections/userTakeAssessment/UserTakeAssessment";
-import AdminSettings from "./ui/pages/admin-settings/AdminSettings";
 
+import Blog from "../src/main/pages/Blog";
+import AssessmentList from "./main/components/sections/userAssessmentList/AssessmentList";
+import AdminAssessmentList from "./ui/pages/admin-settings/adminAssesmentList/AssessmentList";
+import UserTakeAssessment from "./main/components/sections/userTakeAssessment/UserTakeAssessment";
+import Privacy from "./main/pages/Privacy/privacy";
+import EmployeeProfile from "./ui/pages/EmployeeProfile/EmployeeProfile";
+import AdminSetting from "./ui/pages/AdminSetting/AdminSetting";
+import Testimonial from "./main/components/Testimonials/Testimonial";
+import HelpCenter from "../src/main/pages/HelpCenter";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MainLayout>
-        <Routes>
+      <Routes>
+        <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/demo" element={<ScheduleDemo />} />
           <Route path="/confirm-demo" element={<Confirmed />} />
-          <Route path="/contact" element={<Contact />} />
+ 
           <Route path="/support" element={<Support />} />
           <Route path="/termsAndService" element={<Terms />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/skillaquisition" element={<BlogStory />} />
           <Route path="/assessment-list" element={<AssessmentList />} />
+          <Route path="/admin-assessment" element={<AdminAssessmentList />} />
+          <Route
+            path="/admin-assessment-list"
+            element={<AdminAssessmentList />}
+          />
+          <Route path="/help-center" element={<HelpCenter />} />
           <Route
             path="/take-assessment-list"
             element={<UserTakeAssessment />}
           />
-          <Route path="/setting" element={<AdminSettings />} />1 
-        </Routes>
-      </MainLayout>
+          <Route path="/setting" element={<AdminSetting />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
+          <Route path="/employee-profile" element={<EmployeeProfile />} /> 
+        </Route>
 
+        {/* Auth routes */}
+        <Route element={<AdminAuthLayout />}>
+          <Route path="/register" element={<AdminSignup />} />
+          <Route path="/login" element={<AdminLogin />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/reset-password-success" element={<ResetPasswordSuccess />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+        </Route>
+
+      </Routes>
 
       {/* <UserProfile /> */}
       {/* <UiLayout> */}
       {/* ALL APP PAGES SHOULD BE ROUTED WITH THIS LAYOUT COMPONENET */}
-
       {/* </UiLayout> */}
     </ThemeProvider>
   );
