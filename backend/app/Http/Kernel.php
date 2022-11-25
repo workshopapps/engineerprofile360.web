1f2597eaeb42a72207a3a29a7c58a7709daec614
benrobo <alumonabenaiah71@gmail.com>
2022-11-25 04:54:40 -0800

    fixed cors issues

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\CustomCors::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- backend/app/Http/Middleware/CustomCors.php
+++ backend/app/Http/Middleware/CustomCors.php
@@ -19,7 +19,7 @@ class CustomCors
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', '*')
-        ->header('Access-Control-Allow-Credentials', true)
+        ->header('Access-Control-Allow-Credentials', false)
         ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
         ->header('Accept', 'application/json');
     }
