070f17ab5287e77a676d84e2eb7c6a3c318aa405
unknown <wyze97@gmail.com>
2022-11-25 10:05:42 +0100

    feat: Add the pages components

--- frontend/src/main/components/sections/passwordResetSuccessful/ResetSuccessful.jsx
+++ frontend/src/main/components/sections/passwordResetSuccessful/ResetSuccessful.jsx
@@ -1,7 +1,20 @@
+import skriptlogo from "./assets/Skriptlogo.png"
+import successfulImg from "./assets/successful.png"
+
 export default function ResetSuccessful(){
     return(
-        <div>
-            
+        <div className="reset-successful-page">
+            <div className="successful-header">
+                <img src={skriptlogo} alt="skript logo" />
+            </div>
+            <div className="successful-body">
+                <img src={successfulImg} alt="reset successful" />
+                <h1>Successful passsworg reset!</h1>
+                <p>You can now use your new password to log in to your account</p>
+            </div>
+            <div>
+                
+            </div>
         </div>
     )
 }
