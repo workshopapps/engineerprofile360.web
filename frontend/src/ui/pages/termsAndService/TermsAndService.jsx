0c3a2002651c9e735ff2dbbb3156f033f5e92baa
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-11-25 08:15:01 +0100

    fix:fix routing bug for admin-view-assessment page

--- frontend/src/ui/pages/termsAndService/TermsAndService.jsx
+++ frontend/src/ui/pages/termsAndService/TermsAndService.jsx
@@ -4,11 +4,7 @@ import "./style.css";
 
 function TermsAndService() {
   return (
-<<<<<<< HEAD
-    <div className=" mainDiv container">
-=======
     <div className="container mainDiv">
->>>>>>> origin/dev
       <div className="terms">
         <h1>
           Skript <span> Terms of Service</span>
@@ -16,8 +12,6 @@ function TermsAndService() {
         <p>Last updated on November 15, 2022</p>
       </div>
 
-<<<<<<< HEAD
-=======
       <div className="aside col-sm-12 col-md-3 offset-md-1 d-block d-md-none ">
         <h6 className="mx-auto">TABLE OF CONTENT</h6>
         <div>
@@ -108,32 +102,25 @@ function TermsAndService() {
           </div>
         </div>
       </div>
->>>>>>> origin/dev
       <div className=" d-flex">
         <div className="col-sm-12 col-md-8">
           <div className="first">
-            <p>
-              Skript is a tool that provides a subscription service for
-              companies that want to know how good their engineers are. It is a
-              software where engineers register and get various multi-choice
-              questions on different topics. The tool builds a profile of each
-              engineer in the form of a spider-web chart so the company knows
-              where its engineers need to improve. To provide our Services (as
-              defined below) through our software, or website, we need to obtain
-              your agreement to our Terms of Service. These Terms of Service
-              govern your use of our software. As used in these Terms of
-              Service; "Skript service", "our service" "the service", means the
-              personalized service provided by Skript for discovering and
-              assessing Skript content, including all features and
-              functionalities, recommendations and reviews, our websites, and
-              user interfaces, as well as all content and software associated
-              with our service. Skript Evaluator (Skript, ``our``, ``we,`` or
-              ``us ``) provides the services described below to you ("Services")
-              unless you live in a country or territory where it is not
-              eligible. Registration and Account. Your Company/team must create
-              a Business account by providing accurate, current, and complete
-              infor
-            </p>
+            Skript is a tool that provides a subscription service for companies
+            that want to know how good their engineers are. It is a questions on
+            different topics. The tool builds a profile of each engineer in the
+            form of a spider-web chart so the company knows defined below)
+            through our software, or website, we need to obtain your agreement
+            to our Terms of Service. These Terms of Service Service; "Skript
+            service", "our service" "the service", means the personalized
+            service provided by Skript for discovering and assessing Skript
+            content, including all features and functionalities, recommendations
+            and reviews, our websites, and user interfaces, as well as all
+            content and software associated with our service. Skript Evaluator
+            (Skript, ``our``, ``we,`` or ``us ``) provides the services
+            described below to you ("Services") unless you live in a country or
+            territory where it is not eligible. Registration and Account. Your
+            Company/team must create a Business account by providing accurate,
+            current, and complete infor
           </div>
 
           <div className="second">
@@ -385,11 +372,7 @@ function TermsAndService() {
           <div>
             <ul>
               <a href="#RegistrationandAccount">
-<<<<<<< HEAD
-                <li className=" list-none">Registration and Account</li>
-=======
                 <li className="list-none ">Registration and Account</li>
->>>>>>> origin/dev
               </a>
 
               <a href="#Subscription">
@@ -423,11 +406,7 @@ function TermsAndService() {
 
             <div className="button col-12">
               <div className=" wrapper">
-<<<<<<< HEAD
-                <img src={Sms} alt="Sms icon" />
-=======
                 <img src={Sms} alt="sms icon" />
->>>>>>> origin/dev
                 <span>Questions? Leave a message</span>
               </div>
             </div>
