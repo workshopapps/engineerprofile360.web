b0d79a88b974a8a190f6ede05a6ae66c7a9f1395
Edema <edemaero@gmail.com>
2022-11-25 04:21:35 +0100

    FE-74-Feat:Marketing Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { AdminAuthLayout } from "./main/components";
 import {
   Home,
   About,
+  BioMedical,
   AdminSignup,
   AdminLogin,
   ResetPassword,
@@ -38,6 +39,7 @@ const App = () => {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About />} />
+          <Route path="/biomedical-landing" element={<BioMedical />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/demo" element={<ScheduleDemo />} />
           <Route path="/confirm-demo" element={<Confirmed />} />
