3862daab4b494ce7aa8fad645af57ef321081ea8
ricqCodes <princenwakanma1996@gmail.com>
2022-11-26 01:49:39 +0800

    fix bug from merging

--- frontend/src/main/pages/Auth/AdminSignup.jsx
+++ frontend/src/main/pages/Auth/AdminSignup.jsx
@@ -13,6 +13,7 @@ import smsSvg from "../../../assets/icons/smsenvelope.svg";
 import editSvg from "../../../assets/icons/edit-2.svg";
 
 const AdminSignup = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const {
     formData,
     changeInputValue,
@@ -33,178 +34,195 @@ const AdminSignup = () => {
     changeInputValue(e);
   };
 
-  //TODO: MOVE TO UTILS
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  async function Fetch(url, config = {}) {
-    config["headers"] = {
-      "content-type": "application/json",
-    };
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-      try {
-        validation(formData);
-        if (Object.keys(errors).length > 0) {
-          setTouched({
-            fname: true,
-            uname: true,
-            email: true,
-            password: true,
-            confirmPassword: true,
-          });
-        }
-
-        if (Object.keys(errors).length === 0) {
-          setTouched({
-            fname: false,
-            uname: false,
-            email: false,
-            password: false,
-            confirmPassword: false,
-          });
-
-          const {
-            email,
-            fname: full_name,
-            uname: username,
-            password,
-          } = formData;
-          const response = await axios.post(
-            "auth/register",
-            JSON.stringify({ email, full_name, username, password }),
-            {
-              headers: {
-                "content-type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-              },
-              withCredentials: true,
-            }
-          );
-
-          // Clear input fields
-          setFormData({
-            fname: "",
-            uname: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-          });
-        } else if (errors) throw new Error();
-      } catch (err) {
-        // isFormSubmitted.current = false;
-
-        if (!err?.response) {
-        }
+    try {
+      validation(formData);
+      if (Object.keys(errors).length > 0) {
+        setTouched({
+          fname: true,
+          uname: true,
+          email: true,
+          password: true,
+          confirmPassword: true,
+        });
       }
-    };
 
-    const { fname, uname, email, password, confirmPassword } = formData;
+      if (Object.keys(errors).length === 0) {
+        setTouched({
+          fname: false,
+          uname: false,
+          email: false,
+          password: false,
+          confirmPassword: false,
+        });
 
-    return (
-      <>
-        <FormContainer>
-          <AuthTitle title="Sign up" text="Let's get started" />
-          <SignupForm>
-            <InputField
-              $size="md"
-              type="text"
-              label="Full Name"
-              id="fname"
-              placeholder="Jane Doe"
-              value={fname}
-              handleChange={changeInputValue}
-              handleBlur={onBlur}
-              endIcon={<img src={editSvg} alt="" />}
-              helperText={
-                errors && errors.fname && touched.fname ? errors.fname : ""
-              }
-            />
-            <InputField
-              $size="md"
-              type="email"
-              label="Email Address"
-              id="email"
-              placeholder="janedoe@gmail.com"
-              value={email}
-              handleChange={onChange}
-              handleBlur={onBlur}
-              endIcon={<img src={smsSvg} alt="" />}
-              helperText={
-                errors && errors.email && touched.email ? errors.email : ""
-              }
-            />
-            <InputField
-              $size="md"
-              // type={showPassword ? "password" : "text"}
-              label="Password"
-              id="password"
-              placeholder="enter password"
-              value={password}
-              handleChange={onChange}
-              handleBlur={onBlur}
-              endIcon={
-                <img
-                  // onClick={() => setShowPassword((prevState) => !prevState)}
-                  src={eyeSvg}
-                  alt=""
-                />
-              }
-              helperText={
-                errors && errors.password && touched.password
-                  ? errors.password
-                  : ""
-              }
-            />
-            {/* <Spans color={error}>
-            <p>Your Password should have:</p>
-            <span>At least 8 characters</span>
-            <span>At least one capital letter</span>
-            <span>At least one number</span>
-            <span>At least one special character</span>
-          </Spans> */}
-            <InputField
-              $size="md"
-              type="password"
-              label="Confirm Password"
-              id="confirmPassword"
-              placeholder="confirm password"
-              value={confirmPassword}
-              handleChange={onChange}
-              handleBlur={onBlur}
-              error={
-                errors &&
-                touched.confirmPassword &&
-                errors.confirmPassword?.length > 0
-              }
-              endIcon={<img src={eyeSvg} alt="" />}
-              helperText={
-                errors && errors.confirmPassword && touched.confirmPassword
-                  ? errors.confirmPassword
-                  : ""
-              }
-            />
-            <Checkbox>
-              <label>
-                <input type="checkbox" /> I agree to the{" "}
-                <a href="/terms"> Terms and Conditions</a>
-              </label>
-            </Checkbox>
+        const { email, fname: full_name, uname: username, password } = formData;
+        const response = await axios.post(
+          "auth/register",
+          JSON.stringify({ email, full_name, username, password }),
+          {
+            headers: {
+              "content-type": "application/json",
+            },
+          }
+        );
 
-            <Button $size="md" type="submit" onClick={handleSubmit}>
-              Proceed to Signup
-            </Button>
+        // Clear input fields
+        setFormData({
+          fname: "",
+          uname: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        });
+      } else if (errors) throw new Error();
+    } catch (err) {
+      // isFormSubmitted.current = false;
 
-            <div>
-              <span>
-                Already have an account? <Link to="/login">Login</Link>
-              </span>
-            </div>
-          </SignupForm>
-        </FormContainer>
-      </>
-    );
-  }
+      if (!err?.response) {
+      }
+    }
+  };
+
+  const { fname, uname, email, password, confirmPassword } = formData;
+
+  return (
+    <>
+      <FormContainer>
+        <AuthTitle title="Sign up" text="Let's get started" />
+        <SignupForm>
+          <InputField
+            $size="md"
+            type="text"
+            label="Full Name"
+            id="fname"
+            placeholder="Jane Doe"
+            value={fname}
+            handleChange={changeInputValue}
+            handleBlur={onBlur}
+            error={errors && touched.fname && errors.fname?.length > 0}
+            endIcon={<img src={editSvg} alt="" />}
+            helperText={
+              errors && errors.fname && touched.fname ? errors.fname : ""
+            }
+          />
+          <InputField
+            $size="md"
+            type="text"
+            label="User Name"
+            id="uname"
+            placeholder="Jane Doe"
+            value={uname}
+            handleChange={changeInputValue}
+            handleBlur={onBlur}
+            error={errors && touched.uname && errors.uname?.length > 0}
+            endIcon={<img src={editSvg} alt="" />}
+            helperText={
+              errors && errors.uname && touched.uname ? errors.uname : ""
+            }
+          />
+          <InputField
+            $size="md"
+            type="email"
+            label="Email Address"
+            id="email"
+            placeholder="janedoe@gmail.com"
+            value={email}
+            handleChange={onChange}
+            handleBlur={onBlur}
+            error={errors && touched.email && errors.email?.length > 0}
+            endIcon={<img src={smsSvg} alt="" />}
+            helperText={
+              errors && errors.email && touched.email ? errors.email : ""
+            }
+          />
+          <InputField
+            $size="md"
+            type={showPassword ? "text" : "password"}
+            label="Password"
+            id="password"
+            placeholder="enter password"
+            value={password}
+            handleChange={onChange}
+            handleBlur={onBlur}
+            error={errors && touched.password && errors.password?.length > 0}
+            endIcon={
+              <img
+                onClick={() => setShowPassword((prevState) => !prevState)}
+                src={eyeSvg}
+                alt=""
+              />
+            }
+            helperText={
+              errors && errors.password && touched.password
+                ? errors.password
+                : ""
+            }
+          />
+          <Spans>
+            {errors && touched.password ? (
+              <>
+                {errors.passwordLength &&
+                errors.passwordUppercase &&
+                errors.passwordNumber &&
+                errors.passwordCharacter ? (
+                  <p>Your Password should have:</p>
+                ) : (
+                  ""
+                )}
+
+                <span>{errors.passwordLength}</span>
+                <span>{errors.passwordUppercase}</span>
+                <span>{errors.passwordNumber}</span>
+                <span>{errors.passwordCharacter}</span>
+              </>
+            ) : (
+              ""
+            )}
+          </Spans>
+          <InputField
+            $size="md"
+            type={showPassword ? "text" : "password"}
+            label="Confirm Password"
+            id="confirmPassword"
+            placeholder="confirm password"
+            value={confirmPassword}
+            handleChange={onChange}
+            handleBlur={onBlur}
+            error={
+              errors &&
+              touched.confirmPassword &&
+              errors.confirmPassword?.length > 0
+            }
+            endIcon={<img src={eyeSvg} alt="" />}
+            helperText={
+              errors && errors.confirmPassword && touched.confirmPassword
+                ? errors.confirmPassword
+                : ""
+            }
+          />
+          <Checkbox>
+            <label>
+              <input required type="checkbox" /> I agree to the{" "}
+              <a href="/terms"> Terms and Conditions</a>
+            </label>
+          </Checkbox>
+
+          <Button $size="md" type="submit" onClick={handleSubmit}>
+            Proceed to Signup
+          </Button>
+
+          <div>
+            <span>
+              Already have an account? <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </SignupForm>
+      </FormContainer>
+    </>
+  );
 };
 
 export default AdminSignup;
@@ -242,7 +260,7 @@ const Checkbox = styled.div`
 `;
 
 const Spans = styled.div`
-  margin-top: -30px;
+  margin-top: -20px;
   display: flex;
   flex-direction: column;
   align-self: flex-start;
@@ -255,5 +273,6 @@ const Spans = styled.div`
   span {
     font-size: 10px;
     font-family: inherit;
+    color: ${({ theme }) => theme.palette.status.error.color};
   }
 `;
