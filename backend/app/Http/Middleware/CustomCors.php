2b75c4f87a2c09eaea1ff53532920eb6bf3fc431
benrobo <alumonabenaiah71@gmail.com>
2022-11-26 05:22:46 -0800

    chore: updated cors origin

--- backend/app/Http/Middleware/CustomCors.php
+++ backend/app/Http/Middleware/CustomCors.php
@@ -18,7 +18,7 @@ class CustomCors
     {
         // return print_r("welcome");
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->header('Access-Control-Allow-Origin', 'http://localhost:3000, http://skript.hng.tech')
         ->header('Access-Control-Allow-Methods', '*')
         ->header('Access-Control-Allow-Credentials', false)
         ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
