8deefce7a33623b2cc2dc005ac5b7e62437671c9
ricqCodes <princenwakanma1996@gmail.com>
2022-11-27 07:53:04 +0800

    Make validation more reusable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.skript.hng.tech/api/",
   headers: {
-    "content-type": "text/plain",
+    "content-type": "application/json",
   },
 });
