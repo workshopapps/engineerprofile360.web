914022e8bc54bf3cbfe226cbae3af1befae88c33
browynlouis <browynlouis2@gmail.com>
2022-11-26 17:44:15 -0800

    chore: maintaining proper route for assessment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -121,7 +121,6 @@ const App = () => {
         </Route>
 
         <Route element={<UiLayout />}>
-          <Route path="/assessment" element={<Assessment />} /> 
           <Route path="/ui" element={200} />
         </Route>
         <Route element={<DashboardLayout />}>
