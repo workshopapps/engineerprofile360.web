f9ebc329e09c131272ca5cbd9c46eb5692d7db04
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-11-28 22:29:25 +0100

    in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ import UserTakeAssessment from "./main/components/sections/userTakeAssessment/Us
 import Privacy from "./main/pages/Privacy/privacy";
 import EmployeeProfile from "./ui/pages/EmployeeProfile/EmployeeProfile";
 import AdminSetting from "./ui/pages/AdminSetting/AdminSetting";
-import Testimonial from "./main/components/Testimonials/Testimonial";
+// import Testimonial from "./main/components/Testimonials/Testimonial";
 import PricingPage from "./main/pages/PricingPage";
 import HelpCenter from "../src/main/pages/HelpCenter";
 import AdminCSVUpload from "./ui/pages/AdminUpload/AdminCSVUpload";
@@ -51,97 +51,6 @@ import { ComparisonPage } from "./ui/pages/ComparisonPage/ComparisonPage";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/2FA" element={<User2FA />} />
-
-        <Route element={<MainLayout />}>
-          {/* Public routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/biomedical-landing" element={<BioMedical />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/demo" element={<ScheduleDemo />} />
-          <Route path="/confirm-demo" element={<Confirmed />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/help-center" element={<HelpCenter />} />
-          <Route path="/termsAndService" element={<Terms />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/comparison" element={<ComparisonPage />} />
-          <Route path="/assessment-list" element={<AssessmentList />} />
-          <Route path="/admin-assessment" element={<AdminAssessmentList />} />
-
-          <Route
-            path="/admin-assessment-list"
-            element={<AdminAssessmentList />}
-          />
-
-          <Route path="/pricing" element={<PricingPage />} />
-          <Route
-            path="/take-assessment-list"
-            element={<UserTakeAssessment />}
-          />
-
-          <Route
-            path="/user-assessment-result"
-            element={<UserAssessmentResult />}
-          />
-          <Route
-            path="/admin-view-assessment"
-            element={<AdminViewAssessment />}
-          />
-          <Route path="/csv-upload" element={<CsvUpload />} />
-
-          <Route path="/csv-uploading" element={<CsvUploading />} />
-
-          <Route
-            path="/csv-uploading-complete"
-            element={<CsvUploadComplete />}
-          />
-
-          <Route path="/setting" element={<AdminSetting />} />
-          <Route path="/privacy-policy" element={<Privacy />} />
-          <Route path="/employee-profile" element={<EmployeeProfile />} />
-          <Route path="/admin-csv-upload" element={<AdminCSVUpload />} />
-          <Route
-            path="/user-assessment-completed"
-            element={<UserAssessmentListCompleted />}
-          />
-        </Route>
-
-        <Route element={<AdminAuthLayout />}>
-          <Route path="/register" element={<AdminSignup />} />
-          <Route path="/login" element={<AdminLogin />} />
-          <Route path="/reset-password" element={<AdminResetPassword />} />
-          <Route
-            path="/reset-password-success"
-            element={<AdminPasswordSuccess />}
-          />
-          <Route path="/set-password" element={<AdminSetPassword />} />
-          <Route path="/verify-email" element={<AdminVerifyEmail />} />
-          <Route
-            path={"/auth/verify/:user_id/:token"}
-            element={<AdminEmailVerified />}
-          />
-        </Route>
-
-        <Route element={<UiLayout />}>
-          {/* <Route path="/assessment" element={<Assessment />} />  */}
-          <Route path="/ui" element={200} />
-        </Route>
-        <Route element={<DashboardLayout />}>
-          <Route path="/assessment" element={<Assessment />} />
-        </Route>
-      </Routes>
-
-      {/* <UserProfile /> */}
-      {/* <UiLayout> */}
-      {/* ALL APP PAGES SHOULD BE ROUTED WITH THIS LAYOUT COMPONENET */}
-      {/* </UiLayout> */}
-    </ThemeProvider>
-=======
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
@@ -235,7 +144,6 @@ const App = () => {
       </ThemeProvider>
       <StyledToastContainer />
     </>
->>>>>>> 04cab231440c4c1e36799cb09ab5f584152080cb
   );
 };
 export default App;
