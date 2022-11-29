443bedc01c9cd96b81faf2330724b8a667bb4efd
Kojo Baffoe <kojobaffoe935@gmail.com>
2022-11-29 14:33:17 -0500

    refactor: terms and service page

--- frontend/src/ui/pages/termsAndService/TermsAndService.jsx
+++ frontend/src/ui/pages/termsAndService/TermsAndService.jsx
@@ -8,6 +8,8 @@ import Grid, { GridItem } from "../../components/layout/Grid";
 const Page = styled.div`
   padding: 70px 48px;
   background: #fff;
+  margin: auto;
+  max-width: 1440px;
 
   @media screen and (max-width: 1023px) {
     padding: 16px;
