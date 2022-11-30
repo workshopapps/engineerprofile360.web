b9dc0880cecb3af4b749e70ae03712de6c1bb64f
ricqCodes <princenwakanma1996@gmail.com>
2022-11-30 17:09:58 +0800

    Route to dashboard

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+import AuthContext from "../context/authProvider";
 
 const useAuth = () => {
   return useContext(AuthContext);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
-import { AuthProvider } from "./context/AuthProvider";
+import { AuthProvider } from "./context/authProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
