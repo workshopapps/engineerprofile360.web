1e0941fb0b577639a2f84766bbf18a6b6e4a9704
browynlouis <browynlouis2@gmail.com>
2022-11-30 13:13:35 -0800

    styles: added fullWidth prop to Button

--- frontend/src/styles/reusableElements.styled.js
+++ frontend/src/styles/reusableElements.styled.js
@@ -28,11 +28,17 @@ export const Title = styled.h1`
 export const Button = styled.button`
   min-width: 77px;
   min-height: 44px;
-  max-width: 288px;
+  max-width: ${(props) => props.$maxWidth ? props.$maxWidth : "288px"};
   height: ${(props) =>
     props.$size === "md" ? "48px" : props.$size === "lg" ? "80px" : "auto"};
   width: ${(props) =>
-    props.$size === "md" ? "201px" : props.$size === "lg" ? "280px" : "auto"};
+    props.$size === "md"
+      ? "201px"
+      : props.$size === "lg"
+      ? "280px"
+      : props.fullWidth
+      ? "100%"
+      : "auto"};
   font-size: ${(props) =>
     props.$size === "md" ? "14px" : props.$size === "lg" ? "20px" : "auto"};
   display: flex;
@@ -44,6 +50,7 @@ export const Button = styled.button`
     ${({ theme }) => theme.spacing(2.5)};
   cursor: pointer;
   outline: none;
+  font-weight: ${(props) => (props.$weight ? props.$weight : "")};
 
   ${(props) => {
     switch (props.$variant) {
@@ -113,7 +120,7 @@ export const OverlayLoader = styled.div`
   justify-content: center;
   z-index: 10;
   flex-direction: column;
-  gap: ${({theme}) => theme.spacing(2)};
+  gap: ${({ theme }) => theme.spacing(2)};
   
   div {
     border: 8px solid #106ebe;
