37e91727580967d5db387208471b76a82cdfff4a
Priceless-P <prisca.priceless11@gmail.com>
2022-12-02 19:01:20 +0100

    fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -200,7 +200,7 @@ Route::prefix("user-assessment")->group(function () {
 Route::prefix("stack")->group(function () {
     Route::post('add', [StackController::class, 'addStack'])->middleware("isloggedin", "isadmin");
     Route::put('update/{stackId}', [StackController::class, 'updateStack'])->middleware("isloggedin", "isadmin");
-    Route::put('delete/{stackId}', [StackController::class, 'deleteStack']);
+    Route::delete('delete/{stackId}', [StackController::class, 'deleteStack']);
     Route::get('all', [StackController::class, 'getAllStacks']);
     Route::get('{id}', [StackController::class, 'getStackById']);
 });
