1a0524f8e7bad91bc2b22bea26adba9e55519a7d
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-12-03 18:47:52 +0100

    fix: resolving conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -72,7 +72,7 @@ import UserAssessmentListOutlet from "./ui/pages/user-assessment-list/UserAssess
 import EmployeeUserDashboard from "./ui/pages/Employee/EmployeeDashboard";
 import Error from "./ui/pages/404";
 import EmployeeDeparment from "./ui/pages/EmployeeDeparment";
-import GuestEmail from "./main/pages/GuestEmail";
+
 import GuestLogin from "./main/pages/Auth/GuestLogin";
 import DepartmentSection from "./ui/pages/DepartmentSection/DepartmentSection";
 import PersistLogin from "./components/PersistLogin";
@@ -92,7 +92,10 @@ const App = () => {
         <Routes>
           <Route path="/2FA" element={<User2FA />} />
           <Route path="/guest-email" element={<GuestEmail />} />
-          <Route path="/guest-assessment-list" element={<GuestAssessmentList />} />
+          <Route
+            path="/guest-assessment-list"
+            element={<GuestAssessmentList />}
+          />
 
           {/* Public routes */}
           <Route element={<MainLayout />}>
@@ -225,7 +228,7 @@ const App = () => {
               <Route element={<RequireAuth allowedRole={ROLES.Admin} />}>
                 {/* Put in Protected pages in here */}
               </Route>
-                <Route path="/assessment" element={<Assessment />} />
+              <Route path="/assessment" element={<Assessment />} />
             </Route>
           </Route>
         </Routes>
