afe8b4ee425aa28ee82e2d2734f55ee65f13b7c2
ricqCodes <princenwakanma1996@gmail.com>
2022-12-03 23:20:26 +0800

    changes in persistLogin

--- frontend/src/components/PersistLogin.js
+++ frontend/src/components/PersistLogin.js
@@ -10,7 +10,6 @@ const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
   const { setAuth, persist, auth } = useAuth();
-  const isMounted = useRef(true);
 
   // useEffect(() => {
   //   const loggedInUser = localStorage.getItem("EvalOrg");
@@ -19,28 +18,25 @@ const PersistLogin = () => {
   //     loggedInUser !== null && setAuth(JSON.parse(loggedInUser));
   //     setIsLoading(false);
   //   }
-    // return () => (isMounted.current = false);
+  // return () => (isMounted.current = false);
   // }, [isMounted]);
-
 
   useEffect(() => {
     let isMounted = true;
 
-  const verifyRefreshToken = async () => {
-    try {
-      await refresh();
-    } catch (err) {
-      console.error(err);
-    } finally {
-      isMounted && setIsLoading(false);
-    }
-  };
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
 
-  !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
 
-  return () => (isMounted = false);
-
-  eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => (isMounted = false);
   }, []);
 
   return (
