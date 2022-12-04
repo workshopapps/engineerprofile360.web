f000253d64a0387ba13c8c17f7072d4b10cbbcb5
Akinola Farouq Akinkunmi <akinolaakinkunmifa@gmail.com>
2022-12-04 11:12:20 +0100

    used the update migration file to add a new questions column

--- backend/database/migrations/2022_11_16_134040_create_questions_table.php
+++ backend/database/migrations/2022_11_16_134040_create_questions_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->uuid('id')->unique();
             $table->string("question_id");
-            $table->string("question");
             $table->string("options");
             $table->string("timeframe");
             $table->string("correct_answers");
