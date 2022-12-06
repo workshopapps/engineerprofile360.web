860ff4a4175e87c4930b904c0b4d063323cf9100
Uzoigwe Christian <chriswax.ent@gmail.com>
2022-12-06 13:50:56 +0100

    Added null on the getStack and getStackById

--- backend/app/Http/Controllers/StackController.php
+++ backend/app/Http/Controllers/StackController.php
@@ -22,23 +22,33 @@ class StackController extends Controller
             'user_id' => $user_id
         ];
         try {
-            if(empty($name)){
-                return $this->sendResponse(true, 'stack name is required', 'missing stack name',
-                    Response::HTTP_BAD_REQUEST );
+            if (empty($name)) {
+                return $this->sendResponse(
+                    true,
+                    'stack name is required',
+                    'missing stack name',
+                    Response::HTTP_BAD_REQUEST
+                );
             }
             $stack = Stack::where('name', $name);
 
             if ($stack->count() > 0) {
-                return $this->sendResponse(true, "This stack already exists", "Duplicate stack name.",
-                    Response::HTTP_BAD_REQUEST);
+                return $this->sendResponse(
+                    true,
+                    "This stack already exists",
+                    "Duplicate stack name.",
+                    Response::HTTP_BAD_REQUEST
+                );
             }
             $return_message = Stack::create($data);
-                return $this->sendResponse(false, null, $return_message, Response::HTTP_CREATED);
-
-
+            return $this->sendResponse(false, null, $return_message, Response::HTTP_CREATED);
         } catch (\Exception $e) {
-            return $this->sendResponse(true, 'Error occurred while creating Stack', $e->getMessage(),
-                Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->sendResponse(
+                true,
+                'Error occurred while creating Stack',
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
         }
     }
 
@@ -47,12 +57,20 @@ class StackController extends Controller
         try {
 
             if (empty($stack_id)) {
-                return $this->sendResponse(true, "Expected id of an existing stack. None given",
-                    "stack id is missing.", Response::HTTP_BAD_REQUEST);
+                return $this->sendResponse(
+                    true,
+                    "Expected id of an existing stack. None given",
+                    "stack id is missing.",
+                    Response::HTTP_BAD_REQUEST
+                );
             }
             if (empty($request->name)) {
-                return $this->sendResponse(true, "Expected a valid stack 'name'. None give",
-                    "stack name is missing.", Response::HTTP_BAD_REQUEST);
+                return $this->sendResponse(
+                    true,
+                    "Expected a valid stack 'name'. None give",
+                    "stack name is missing.",
+                    Response::HTTP_BAD_REQUEST
+                );
             }
 
             // Get current user
@@ -64,67 +82,82 @@ class StackController extends Controller
             $user_id = $stack->first()["user_id"];
 
             if ($user_id !== $uid) {
-                return $this->sendResponse(true, "Not authorised to update this stack",
-                    "Unauthorised.", Response::HTTP_BAD_REQUEST);
+                return $this->sendResponse(
+                    true,
+                    "Not authorised to update this stack",
+                    "Unauthorised.",
+                    Response::HTTP_BAD_REQUEST
+                );
             }
 
             if ($stack->count() == 0) {
-                return $this->sendResponse(true, "Stack doesn't exists", "Stack not found.",
-                    Response::HTTP_BAD_REQUEST);
+                return $this->sendResponse(
+                    true,
+                    "Stack doesn't exists",
+                    "Stack not found.",
+                    Response::HTTP_BAD_REQUEST
+                );
             }
 
             $stack->where("user_id", $uid)->update($updated_name);
 
-            return $this->sendResponse(false, null, 'Stack updated successfully',
-                $updated_name, Response::HTTP_OK);
+            return $this->sendResponse(
+                false,
+                null,
+                'Stack updated successfully',
+                $updated_name,
+                Response::HTTP_OK
+            );
         } catch (Exception $e) {
 
-            return $this->sendResponse(true, "Something went wrong updating stack " . $e->getMessage(),
-                'Failed updating stack.', Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->sendResponse(
+                true,
+                "Something went wrong updating stack " . $e->getMessage(),
+                'Failed updating stack.',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
         }
-
     }
 
-     //function that gets all stacks
-     public function getAllStacks(): JsonResponse
-     {
-         try{
-             $stack = Stack::get();
-             if($stack){
-                 return $this->sendResponse(false, "All Stack Fetched", $stack, Response::HTTP_OK);
-             }else{
-                 return $this->sendResponse(true, "Invalid Request", "Stack(s) Not Found",  Response::HTTP_NOT_FOUND);
-             }
-         } catch(Exception $e){
-             return $this->sendResponse(true, "Error Occured while trying to fetch all stack from DB", $e->getMessage());
-         }
+    //function that gets all stacks
+    public function getAllStacks(): JsonResponse
+    {
+        try {
+            $stack = Stack::get();
+            if ($stack) {
+                return $this->sendResponse(false, null, "All Stack Fetched", $stack, Response::HTTP_OK);
+            } else {
+                return $this->sendResponse(true, "Invalid Request", "Stack(s) Not Found",  Response::HTTP_NOT_FOUND);
+            }
+        } catch (Exception $e) {
+            return $this->sendResponse(true, "Error Occured while trying to fetch all stack from DB", $e->getMessage());
+        }
+    }
 
-     }
-
-     //function that gets Stack by id
+    //function that gets Stack by id
     public function getStackById($id)
     {
-        try{
+        try {
             $stackId = Stack::find($id);
-            if(!$stackId){
+            if (!$stackId) {
                 return $this->sendResponse(true, "Invalid Request", "No Stack with this id in DB", Response::HTTP_NOT_FOUND);
             }
-            return $this->sendResponse(false, "Stack Fetched", $stackId, Response::HTTP_OK);
-        } catch(Exception $e){
-                return $this->sendResponse(true, "Error occurred while fetching Stack", $e->getMessage());
-         }
+            return $this->sendResponse(false, null, "Stack Fetched", $stackId, Response::HTTP_OK);
+        } catch (Exception $e) {
+            return $this->sendResponse(true, "Error occurred while fetching Stack", $e->getMessage());
+        }
     }
 
 
     // delete stack
     public function deleteStack($stack_id): JsonResponse
     {
-        try{
+        try {
 
             // Get stack by id
             $stack = Stack::find($stack_id);
 
-            if( !$stack ) {
+            if (!$stack) {
                 return $this->sendResponse(
                     'Stack does not exist',
                     'Stack not found',
@@ -135,8 +168,8 @@ class StackController extends Controller
             $stack->delete();
 
             // success response
-            return $this->sendResponse(true, 'Stack deleted successfully',[], Response::HTTP_OK);
-        }  catch (Exception $e) {
+            return $this->sendResponse(true, 'Stack deleted successfully', [], Response::HTTP_OK);
+        } catch (Exception $e) {
             return $this->sendResponse('Stack not fetched', $e->getMessage());
         }
     }
