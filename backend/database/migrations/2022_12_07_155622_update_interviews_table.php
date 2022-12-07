5ac74d4feefcd8a9efdbc2b8551d5605336bc3cf
Akinola Farouq Akinkunmi <akinolaakinkunmifa@gmail.com>
2022-12-07 17:42:50 +0100

    Feat(addInterview)- added the title of the assessment and dropped the company and name column

---
+++ backend/database/migrations/2022_12_07_155622_update_interviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('interviews', function (Blueprint $table) {
+            $table->dropColumn('company_name');
+            $table->dropColumn('name');
+            $table->string('times_taken')->nullable()->change();
+            $table->string('title')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
