3e2dceee138597c0a983e862c2dcef6eea19ef97
Beyond-Logic <edemaero@gmail.com>
2022-12-07 08:51:35 +0100

    FE-180:feature - Code Refactor/Redesign

--- frontend/src/ui/components/Company/Assessments/adminAssesmentList/AssessmentList.jsx
+++ frontend/src/ui/components/Company/Assessments/adminAssesmentList/AssessmentList.jsx
@@ -1,21 +1,27 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import styled from "styled-components";
 import { Button, Loader } from "../../../../../styles/reusableElements.styled";
 import addCircle from "../../../../../assets/icons/app/add-circle.svg";
-import down from "../../../../../assets/icons/app/arrow-down-alt.svg";
-import dashboard from "../../../../../assets/icons/app/dashboard.svg";
-import hamburger from "../../../../../assets/icons/app/hamburger.svg";
 import PageInfo from "../../../molecules/PageInfo";
 import Flex from "../../../layout/Flex";
 import { Link } from "react-router-dom";
 import axios from "../../../../../api/axios";
 import useAuth from "../../../../../hooks/useAuth";
+import {
+  AssessmentListings,
+  AssessmentTab,
+  AssessmentTabContainer,
+  ButtonContainer,
+  ButtonInner,
+  Hide,
+  Show,
+  TableContainer,
+  Text,
+  TH1,
+  TH2,
+} from "./AdminAssessmentListAvailable";
+import { showErrorToast } from "../../../../../helpers/helper";
 
 const DataContext = createContext(null);
-
-const fetchCompleted = () => {
-  return axios("/user-assessment/org/org-completed");
-};
 
 const info = [
   {
@@ -77,18 +83,10 @@ const info = [
 ];
 
 export const Buttons = () => {
-  const Mailto = ({ email, subject = "", body = "", children }) => {
-    let params = subject || body ? "?" : "";
-    if (subject) params += `subject=${encodeURIComponent(subject)}`;
-    if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
-
-    return <a href={`mailto:${email}${params}`}>{children}</a>;
-  };
-
   return (
-    <div>
+    <ButtonContainer>
       <Hide>
-        <Flex jc="flex-end" spacing={10}>
+        <ButtonInner>
           <Link to="/assessment/create-assessment">
             <Button>
               <Flex spacing={10} ai="center">
@@ -97,14 +95,7 @@ export const Buttons = () => {
               </Flex>
             </Button>
           </Link>
-          <Mailto email="employee@email.com" subject="ASSESSMENTS">
-            <Button $variant="outlined">
-              <Text $color="#2667FF" $weight="600">
-                Send to employee
-              </Text>
-            </Button>
-          </Mailto>
-        </Flex>
+        </ButtonInner>
       </Hide>
       <Show>
         <Flex jc="flex-end" spacing={10}>
@@ -115,118 +106,36 @@ export const Buttons = () => {
               </Flex>
             </Button>
           </Link>
-          <Mailto email="employee@email.com" subject="ASSESSMENTS">
-            <Button $variant="outlined">
-              <Text $color="#2667FF" $weight="600">
-                Send to employee
-              </Text>
-            </Button>
-          </Mailto>
         </Flex>
       </Show>
-    </div>
-  );
-};
-export const Sort = () => {
-  const { completed, setCompleted, data, setData, order, setOrder } =
-    useContext(DataContext);
-  const sorting = () => {
-    if (order === "asc") {
-      const sorted = [...completed.data].sort((a, b) =>
-        a.dept.toLowerCase() > b.dept.toLowerCase() ? 1 : -1
-      );
-      setCompleted(sorted);
-      setOrder("dsc");
-    }
-    if (order === "dsc") {
-      const sorted = [...completed.data].sort((a, b) =>
-        a.dept.toLowerCase() < b.dept.toLowerCase() ? 1 : -1
-      );
-      setCompleted(sorted);
-      setOrder("asc");
-    }
-  };
-
-  const dateSort = () => {
-    if (order === "asc") {
-      const sortedDate = [...completed.data].sort((a, b) =>
-        new Date(b.date) > new Date(a.date) ? 1 : -1
-      );
-      setCompleted(sortedDate);
-      setOrder("dsc");
-    }
-    if (order === "dsc") {
-      const sortedDate = [...completed.data].sort((a, b) =>
-        new Date(b.date) < new Date(a.date) ? 1 : -1
-      );
-      setCompleted(sortedDate);
-      setOrder("asc");
-    }
-  };
-  return (
-    <SortContainer>
-      <Flex
-        spacing={10}
-        style={{
-          border: "1px solid #8A8886",
-          padding: "8px",
-          borderRadius: "4px",
-        }}
-        ai="center"
-        onClick={() => sorting()}
-      >
-        <Text> Assessment Type</Text>
-        <img src={down} />
-      </Flex>
-      <Flex ai="center" spacing={30}>
-        <Flex
-          spacing={10}
-          style={{
-            border: "1px solid #8A8886",
-            padding: "8px",
-            borderRadius: "4px",
-          }}
-          ai="center"
-          onClick={() => dateSort()}
-        >
-          <Text>Sort By Date</Text>
-
-          <img src={down} />
-        </Flex>
-        <Hide>
-          <Flex spacing={10}>
-            <img src={dashboard} alt="" />
-            <img src={hamburger} alt="" />
-          </Flex>
-        </Hide>
-      </Flex>
-    </SortContainer>
+    </ButtonContainer>
   );
 };
 
 export const List = () => {
   const { completed, setCompleted, isLoading, setIsLoading } =
     useContext(DataContext);
-  const { auth, setAuth } = useAuth();
-
-  const fetchCompleted = () => {
-    return axios("/user-assessment/org/{auth.id}/org-completed");
-  };
+  const { auth } = useAuth();
 
   useEffect(() => {
-    fetchCompleted()
-      .then(({ data }) => {
-        setCompleted(data);
-        console.log(auth);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
+    const getCompletedAssessment = async () => {
+      try {
+        const response = await axios.get(
+          `/assessment/completed-assessments/3ed2cd12-f571-4570-bc8b-f388dd301f67/${auth.id}`
+        );
+        setCompleted(response.data);
         setIsLoading(false);
-        console.log();
-      });
-  }, []);
+      } catch (err) {
+        if (!err?.response) {
+          showErrorToast("No Server Response");
+        } else if (err?.response.data.errorState === true) {
+          showErrorToast(err.response.data.message);
+        }
+      }
+    };
+    getCompletedAssessment();
+  }, [auth.id, setCompleted, setIsLoading]);
+
   const renderContent = () => {
     if (isLoading) {
       return (
@@ -234,43 +143,53 @@ export const List = () => {
           <Loader />
         </Flex>
       );
-    } else if (completed.data.length === 0) {
-      return (
-        <Text>
-          There are no completed assessments, check for available ones and
-          complete
-        </Text>
-      );
     }
+
+    // else if (completed.data.length === 0) {
+    //   return (
+    //     <Text>
+    //       There are no completed assessments, check for available ones and
+    //       complete
+    //     </Text>
+    //   );
+    // }
     return (
-      <table>
-        <tbody>
-          <tr>
-            <th>#</th>
-            <th colSpan="2">Department</th>
-            <th>Course</th>
-            <th>Duration</th>
-            <th>Deadline</th>
-            <th>{""}</th>
-          </tr>
-          {[completed.data].map((d, idx) => {
-            return (
-              <tr key={idx}>
-                <td>{idx + 1}</td>
-                <td colSpan="2">{d.dept}</td>
-                <td>{d.course}</td>
-                <td>{d.duration}</td>
-                <td>{d.date}</td>
-                <td>
-                  <Button $variant="outlined" $color="#2667ff">
-                    Take Test
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TableContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <TH1>Assessment Name</TH1>
+              <th>Department</th>
+              <th>Accepted</th>
+              <th>Duration</th>
+              <th>Deadline</th>
+              <th></th>
+            </tr>
+          </thead>
+          {/* [available.data] */}
+          <tbody>
+            {info.map((d, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <TH2>{d.dept}</TH2>
+                  <td>{d.course}</td>
+                  <td>{d.duration}</td>
+                  <td>{d.course}</td>
+                  <td>{d.date}</td>
+
+                  <td>
+                    <Button $variant="outlined" $color="#2667ff">
+                      View Assessment
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </TableContainer>
     );
   };
   return <AssessmentListings>{renderContent()}</AssessmentListings>;
@@ -278,35 +197,29 @@ export const List = () => {
 
 export const TableSection = () => {
   return (
-    <Flex stack spacing={16}>
-      <Sort />
+    <>
       <List />
-    </Flex>
+    </>
   );
 };
 
 export const Assessment = () => {
   return (
-    <Flex stack spacing={70}>
-      <Flex
-        style={{
-          paddingBottom: "10px",
-          paddingTop: "20px",
-          borderBottom: "1px solid #8A8886",
-        }}
-        jc="space-between"
-      >
-        <Flex spacing={24} ai="flex-end">
+    <>
+      <AssessmentTabContainer>
+        <AssessmentTab>
           <Link to="/assessment/assessment-list">
             <Text>Available (0)</Text>
           </Link>
-          <Text $color="#2667FF" $weight="600">
-            Completed (0)
-          </Text>
-        </Flex>
-      </Flex>
+          <Link to="/assessment/assessment-list/completed">
+            <Text $color="#2667FF" $weight="600">
+              Completed (0)
+            </Text>
+          </Link>
+        </AssessmentTab>
+      </AssessmentTabContainer>
       <TableSection />
-    </Flex>
+    </>
   );
 };
 
@@ -329,7 +242,7 @@ export const CompletedAssessmentList = () => {
           setIsLoading,
         }}
       >
-        <PageInfo breadcrumb={["Dashboard", "Performance"]} />
+        <PageInfo breadcrumb={["Dashboard", "Assessment list"]} />
         <Buttons />
         <Assessment />
       </DataContext.Provider>
@@ -338,72 +251,3 @@ export const CompletedAssessmentList = () => {
 };
 
 export default CompletedAssessmentList;
-
-export const AssessmentListings = styled.div`
-  padding-top: ${({ theme }) => theme.spacing(3)};
-  width: 100%;
-  overflow: auto;
-  table {
-    table-layout: fixed;
-    width: 100%;
-    min-width: 960px;
-    overflow: auto;
-
-    tr:first-of-type {
-      width: 100%;
-      background: #f8fbfd;
-
-      th:first-of-type {
-        padding-right: 24px;
-      }
-
-      th {
-        font-size: 16px;
-        font-weight: 600;
-        color: #605e5c;
-      }
-    }
-
-    td {
-      color: #605e5c;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 22px;
-      white-space: nowrap;
-    }
-  }
-`;
-
-export const Text = styled.p`
-  white-space: nowrap;
-  color: ${({ $color }) => ($color ? $color : "initial")};
-  font-size: ${({ $size }) => ($size ? $size : "14px")};
-  font-weight: ${({ $weight }) => ($weight ? $weight : "400")};
-  line-height: ${({ $lHeight }) => ($lHeight ? $lHeight : "20px")};
-`;
-
-export const Hide = styled.div`
-  display: block;
-
-  @media screen and (max-width: 768px) {
-    display: none;
-  } ;
-`;
-export const Show = styled.div`
-  display: none;
-
-  @media screen and (max-width: 768px) {
-    display: block;
-  } ;
-`;
-
-export const SortContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 10px;
-
-  @media screen and (max-width: 1023px) {
-    display: flex;
-    padding: 0;
-  } ;
-`;
