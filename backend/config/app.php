eba32832617c6d5924a8815d66c26a0cdbf4cf69
dasaint001 <elijahbamidele48@yahoo.com>
2022-12-08 09:47:46 +0100

    fix: fixed errors on the application due to bug PR made

--- backend/config/app.php
+++ backend/config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -210,7 +210,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'Helper' => App\Helper\Helper::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
