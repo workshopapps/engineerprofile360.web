845614f77b0a44169b67533ad52b22b56d3dac77
dasaint001 <elijahbamidele48@yahoo.com>
2022-12-10 18:04:08 +0100

    fix: fixed errors

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\QuestionsController;
 use App\Http\Controllers\UserScoreController;
 use App\Http\Controllers\AssessmentController;
 use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\StackController;
 use App\Http\Controllers\AuthenticateController;
 use App\Http\Controllers\UserAssessmentController;
 use App\Helper\Helper;
@@ -30,8 +32,6 @@ use App\Helper\Helper;
 |
 */
 
- // Imported routes.
- include 'api/admin.php';
 
 // other route functions here
 Route::get("/test", function (Helper $helper) {
@@ -88,6 +88,31 @@ Route::prefix("assessment")->group(function () {
     Route::delete('/{assessmentId}/delete', [AssessmentController::class, 'deleteAssessment'])->middleware("isloggedin", "isadmin");
     Route::get('/{companyId}/{orgId}/accepted-assessments', [AssessmentController::class, 'getCompanyAcceptedAssessments'])->middleware("isloggedin", "isadmin");
     Route::get('/completed-assessments/{companyId}/{orgId}', [AssessmentController::class, 'getCompanyCompletedAssessments'])->middleware("isloggedin", "isadmin");
+});
+
+//Admin operation routes
+Route::prefix("admin")->group(function () {
+    Route::get('overview', [AdminController::class, 'getAdminOverview'])->middleware("isloggedin", "isadmin");
+    Route::get('users', [AdminController::class, 'getAllUsers'])->middleware("isloggedin", "isadmin");
+    Route::delete('/{companyId}/delete', [AdminController::class, 'deleteUserCompany']);
+    Route::get('/companies', [AdminController::class, 'getAllCompanies']);
+    Route::get('/employees', [AdminController::class, 'getAllEmployees']);
+
+    //Admin Assessment routes operations
+    Route::prefix("assessment")->group(function () {
+        Route::post('/create', [AssessmentController::class, 'adminCreateAssessment'])->middleware("isloggedin", "isoveralladmin");
+        Route::put('/{assessmentId}', [AssessmentController::class, 'adminUpdateAssessment'])->middleware("isloggedin", "isoveralladmin");
+        Route::delete('/{assessmentId}/delete', [AssessmentController::class, 'adminDeleteAssessment'])->middleware("isloggedin", "isoveralladmin");
+    });
+
+    // Stack route
+    Route::prefix("stack")->group(function () {
+        Route::post('add', [StackController::class, 'addStack'])->middleware("isloggedin", "isadmin");
+        Route::put('{stackId}/update', [StackController::class, 'updateStack'])->middleware("isloggedin", "isadmin");
+        Route::delete('{stackId}/delete', [StackController::class, 'deleteStack']);
+        Route::get('all', [StackController::class, 'getAllStacks'])->middleware("isloggedin", "isadmin");
+        Route::get('{stackId}', [StackController::class, 'getStackById'])->middleware("isloggedin", "isadmin");
+    });
 });
 
 
