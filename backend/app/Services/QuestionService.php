93632fc27c6171d8b4cfb68637c2ece4aa966a87
chisainty <johnchinedudivine@gmail.com>
2022-12-11 05:21:25 +0100

    fix(BE): fixed the issue with the question CSV upload

--- backend/app/Services/QuestionService.php
+++ backend/app/Services/QuestionService.php
@@ -9,25 +9,28 @@ class QuestionService
 {
     public static function uploadQuestions(array $payload): array
     {
-        $base64 = $payload['base64'];
+        $base64 = str_replace(" ", "", $payload['base64']);
         $type = explode(",", $base64)[0];
         // if ($type != "") return;
-        $data = explode("\n", base64_decode(explode(",", $base64)[1]));
+        $data = explode("\n", trim(base64_decode(explode(",", $base64)[1])));
 
         $success = 0;
         $count = count($data);
         for ($i = 1; $i < $count; $i++) {
-            $item = explode(",", $data[$i]);
-            $option1 = trim($item[1]) ?? "option1";
-            $option2 = trim($item[2]) ?? "option2";
-            $option3 = trim($item[3]) ?? "option3";
-            $option4 = trim($item[4]) ?? "option4";
-            $answer = trim($item[5]) ?? "option";
-            $category_name = trim($item[6]) ?? null;
+            //explode each line
+            $item = explode("],", str_replace(']"', "]", $data[$i]));
+            //assign varibles removing the starting and ending square brackets
+            $option1 = rtrim(ltrim($item[1], "["), "]") ?? "option1";
+            $option2 = rtrim(ltrim($item[2], "["), "]") ?? "option2";
+            $option3 = rtrim(ltrim($item[3], "["), "]") ?? "option3";
+            $option4 = rtrim(ltrim($item[4], "["), "]") ?? "option4";
+            $answer = rtrim(ltrim($item[5], "["), "]") ?? "option";
+            $category_name = rtrim(ltrim($item[6], "["), "]") ?? null;
+
             $category = Category::where(["name" => $category_name, "org_id" => $payload['org_id']])->first();
             if ($category) {
                 $output = [
-                    "question" => trim($item[0]) ?? "Question",
+                    "question" => rtrim(ltrim($item[0], "["), "]") ?? "Question",
                     "options" => [$option1, $option2, $option3, $option4],
                     "category_id" => $category['id'],
                     "assessment_id" => $payload['assessment_id'],
