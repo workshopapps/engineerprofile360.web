2267efc515cfc405294333b694e4292fff47a092
Beyond-Logic <edemaero@gmail.com>
2022-12-12 21:57:49 +0100

    FE-Feat:Employee Dashboard Assessment Preview

--- frontend/src/ui/pages/Employee/index.js
+++ frontend/src/ui/pages/Employee/index.js
@@ -2,3 +2,4 @@ export { default as EmployeeLogin } from "./Auth/Login";
 export { default as EmployeeDashboard } from "./Dashboard";
 export { default as EmployeeUserAssessmentListOutlet } from "./EmployeeAssessmentListOutlet";
 export { default as EmployeeUserAssessmentResult } from "./EmployeeAssessmentResult";
+export {default as EmployeePreviewAssessment} from './EmployeePreviewAssessment'
