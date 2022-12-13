38bf246f1e8b1e277e2df45d303d1ff345bdc0ba
browynlouis <browynlouis2@gmail.com>
2022-12-13 11:51:39 -0800

    feat: added assessment result modal

--- frontend/src/ui/components/Employee/Assessments/Listings/Completed.jsx
+++ frontend/src/ui/components/Employee/Assessments/Listings/Completed.jsx
@@ -6,50 +6,70 @@ import { Button } from "../../../../../styles/reusableElements.styled";
 import TableComponent from "../../../molecules/TableComponent";
 import NoData from "../../../molecules/NoData";
 
+import { EmployeeAssessmentResult } from "../..";
+
 const Completed = () => {
   const [completedAssessments, setCompletedAssessments] = useState([]);
+  const [assessmentId, setAssessmentId] = useState();
+  const [modal, setModal] = useState(false);
   const { completed } = useOutletContext();
 
   useEffect(() => {
     setCompletedAssessments(completed ? completed : []);
   }, [completed]);
 
+  const handleClick = (assessment_id) => {
+    setAssessmentId(assessment_id);
+    setModal(true);
+  };
+
   return (
     <>
       <CompletedContainer>
         {completedAssessments.length > 0 ? (
-          <TableComponent>
-            <tbody>
-              <tr>
-                <th>#</th>
-                <th>Assessment</th>
-                <th>Department</th>
-                <th>Starts</th>
-                <th>Deadline</th>
-                <th></th>
-              </tr>
-              {completedAssessments.map((item, index) => (
-                <tr key={item.assessment.id}>
-                  <td>{index + 1}</td>
-                  <td>{item.assessment.name}</td>
-                  <td>{item.assessment.department.name}</td>
-                  <td>
-                    {item.assessment.start_date} - {item.assessment.start_time}
-                  </td>
-                  <td>
-                    {item.assessment.end_date} - {item.assessment.end_time}
-                  </td>
-                  <td>
-                    <Link to="/">
-                      <Button $variant="outlined" $color="#2667ff">
+          <>
+            <TableComponent>
+              <tbody>
+                <tr>
+                  <th>#</th>
+                  <th>Assessment</th>
+                  <th>Department</th>
+                  <th>Starts</th>
+                  <th>Deadline</th>
+                  <th></th>
+                </tr>
+                {completedAssessments.map((item, index) => (
+                  <tr key={item.assessment.id}>
+                    <td>{index + 1}</td>
+                    <td>{item.assessment.name}</td>
+                    <td>{item.assessment.department.name}</td>
+                    <td>
+                      {item.assessment.start_date} -{" "}
+                      {item.assessment.start_time}
+                    </td>
+                    <td>
+                      {item.assessment.end_date} - {item.assessment.end_time}
+                    </td>
+                    <td>
+                      <Button
+                        $variant="outlined"
+                        $color="#2667ff"
+                        onClick={() => handleClick(item.assessment.id)}
+                      >
                         View Result
                       </Button>
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </TableComponent>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </TableComponent>
+            {modal && (
+              <EmployeeAssessmentResult
+                assessment_id={assessmentId}
+                setModal={setModal}
+              />
+            )}
+          </>
         ) : (
           <NoData text="Oops! No Completed Assessments"></NoData>
         )}
--- frontend/src/ui/components/Employee/index.js
+++ frontend/src/ui/components/Employee/index.js
@@ -5,7 +5,7 @@ export { default as Stats } from "./Dashboard/Stats";
 export { default as RecentAssessment } from "./Dashboard/RecentAssessment";
 export { default as AssessmentHeader } from "./Assessment/AssessmentHeader";
 
-
+export { default as EmployeeAssessmentResult } from "./Assessments/Details/AssessmentResult";
 
 // export { default as AssessmentHeader } from "../../../../miscellaneous/TakeAssessment/AssessmentHeader";
 // export { default as AssessmentResultHeader } from "../../../../miscellaneous/TakeAssessment/AssessmentResultHeader";
